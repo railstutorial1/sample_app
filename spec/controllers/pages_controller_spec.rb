a489e913ca76db9c214904e0ffcf1f6201010e42
Mahalakshmi Rupesh <mahalakshmi.rupesh@gmail.com>
2017-06-13 14:55:03 +0530

    Update Gemfile for Heroku

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
     end
+
+    it "should have the right title" do
+      get :home
+      response.body.should have_selector("title", 
+          "Ruby on Rails Tutorial Sample App ! Home")
+    end
   end
 
   describe "GET #contact" do
     it "returns http success" do
       get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
       expect(response).to have_http_status(:success)
     end
   end
